d5e8addb9ea948f3a7fb17755b4f55349c1f1ca5
Klukvas <pavlenkoandrey56@gmail.com>
2023-12-12 21:34:26 +0200

    fix route

--- src/routers/telegram.ts
+++ src/routers/telegram.ts
@@ -1,11 +1,8 @@
-import express, { Request, Response, Router } from 'express'
+import express, { Request, Response } from 'express'
 import { newTelegramMessageController } from '../controllers/new-telegram-message-controller'
 
 const telegramRouter = express.Router()
-telegramRouter.post('/new-message', newTelegramMessageController)
-telegramRouter.get('/', async (req: Request, res: Response) => {
-    res.status(200).send('Ok')
-})
+telegramRouter.post('/', newTelegramMessageController)
 
 export default telegramRouter
 
