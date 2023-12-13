b25adc865d30e170c7c240822d1928d181862d15
Klukvas <pavlenkoandrey56@gmail.com>
2023-12-13 22:04:15 +0200

    change dbg url pars

--- src/utils/core/db.ts
+++ src/utils/core/db.ts
@@ -4,9 +4,8 @@ import { appLogger } from "./logger";
 
 
 async function connectToDb() {
-  const db_url = `${env.dbUrl}/${env.dbName}`
-  appLogger.info(`db_url: ${db_url}`)
-  await mongoose.connect(db_url)
+  appLogger.info(`db_url: ${env.dbUrl}`)
+  await mongoose.connect(env.dbUrl)
   const db = mongoose.connection;
 
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
