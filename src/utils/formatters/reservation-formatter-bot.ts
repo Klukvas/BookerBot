11c12dc5762386ede2e2c9f3f674ae489bc58cc4
Klukvas <pavlenkoandrey56@gmail.com>
2023-12-16 01:08:48 +0200

    fix reservationFormatterBot

--- src/utils/formatters/reservation-formatter-bot.ts
+++ src/utils/formatters/reservation-formatter-bot.ts
@@ -6,14 +6,13 @@ export async function reservationFormatterBot(reservations: IReserved[]){
   let delimiter = '-'.repeat(12)
   for(const item of reservations){
     const seat = await Seat.findOne({_id: item.seatId})
-    const price = calculatePrice({price: seat!.cost, duration: item.duration})
     const reservation=`
       ${delimiter}\n
       \tid: ${item._id}\n
       \tпродолжительность: ${item.duration}\n
       \tзарезервировано на: ${item.reservedFrom}\n
       \tместо №: ${seat!.seatNumber}
-      \t**Итого к оплате**: ${price}
+      \t**Итого к оплате**: ${item.totalAmountToPay}
     `
     formattedText += reservation
   }
